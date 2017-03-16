38d667f304b1364c21bdcebdf22a44870afe2ebc
Scott <smclendening@gmail.com>
2017-03-16 16:03:30 -0700

    add username prompt when component mounts; delete App.jsx file

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
 
 class App extends React.Component {
 	constructor(props){
@@ -10,7 +11,29 @@ class App extends React.Component {
 
 		}
 	}
-	render(){
+
+	componentDidMount() {
+		this.promptUsername;
+	}
+
+	promptUsername() {
+		var username = prompt('What is your username?');
+
+		$.ajax({
+			url: 'http://localhost:3000/users',
+			method: 'POST',
+			headers: {'content-type': 'application/json'},
+			data: JSON.stringify('username': username),
+			success: (data) => {
+				console.log('added user to users DB');
+			},
+			error: (err) => {
+				console.log('error in username POST: ', err);
+			}
+		});
+	}
+
+	render() {
 		return (
 			<h1>Oranges To Oranges</h1>
 		);
