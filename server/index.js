346fbab1b2fd3d6fbdbdfa844cb46873b346b9bb
Scott <smclendening@gmail.com>
2017-03-16 12:25:03 -0700

    (refactor) allow server to serve up static index files

--- server/index.js
+++ server/index.js
@@ -2,9 +2,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-  res.send('<h1>Hello World!</h1>');
-});
+app.use(express.static(__dirname + '/../client/dist'));
 
 app.listen(port, function() {
 	console.log('App is listening on port: ', port);
