b48e29cad60a96c184ee18a5f3d91729b166e21a
Neil Ricci <iccir919@gmail.com>
2017-03-16 14:32:29 -0700

    Add App component to index

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,1 +1,26 @@
 // test
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+
+		}
+	}
+	render(){
+		return (
+			<h1>Oranges To Oranges</h1>
+		);
+	}
+}
+
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('app')
+);
+
+
