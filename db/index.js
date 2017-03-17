2a1b3d56bc137dab17476a478ad7a4c225f5aefb
Scott <smclendening@gmail.com>
2017-03-16 19:20:36 -0700

    Fix typo in db file

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/greenfield');
 
-var db = mongoose.conncetion;
+var db = mongoose.connection;
 
 db.on('error', function() {
   console.log('mongoose connection error');
