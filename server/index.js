a1bb6298e36996fcc886b62c47969cda0f34778d
Hack Reactor Students <accounts@hackreactor.com>
2017-03-20 12:28:08 -0700

    (feat) add passport logic for serializing and deserializing user

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 var bodyParser = require('body-parser');
 var models = require('../db/index.js');
+var User = models.userModel;
 var mongoose = require('mongoose');
 var passport = require('passport');
 var session = require('express-session');
@@ -28,12 +29,22 @@ app.use(passport.initialize());
 
 app.use(passport.session());
 
+// Grabbing name from user
+passport.serializeUser(function(user, done){
+  done(null, user.username);
+});
+
+passport.deserializeUser(function(username, done) {
+  User.find({username: username}).exec().then(function(user){
+    done(null, user.public());
+  })
+});
 
 app.use(express.static(__dirname + '/../client/dist'));
 
 app.post('/users', function (req, res) {
   var username = req.body.username;
-  var user = new models.userModel({
+  var user = new User({
   	username: username
   });
   user.save(function(err) {
@@ -43,8 +54,9 @@ app.post('/users', function (req, res) {
   		res.status(201).send('success')
   	}
   })
-
 });
+
+
 
 var server = app.listen(port, function() {
   console.log('App is listening on port: ', port);
