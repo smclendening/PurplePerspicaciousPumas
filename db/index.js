5226819024342b6aa4fcbf7059acde689f7b8dea
Hack Reactor Students <accounts@hackreactor.com>
2017-03-20 11:47:56 -0700

    Update user model with authentication methods

--- db/index.js
+++ db/index.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
 mongoose.connect('mongodb://localhost/greenfield');
 var Schema = mongoose.Schema;
 
@@ -22,8 +23,45 @@ var gameInstanceSchema = new Schema({
 });
 
 var userSchema = new Schema({
-	username: {type: String, unique: true }
+	username: {type: String, unique: true },
+  password: String,
+  hash: String,
+  email: {type: String, unique: true},
+  attempts: {type: Number, default: 0}
 });
 
+userSchema.statics.public = function() {
+  delete this.hash;
+  return this;
+};
+
+userSchema.statics.authenticate = function(password) {
+  if (bcrypt.compareSync(password, this.hash) && this.attempts < 20) {
+    this.attempts = 0;
+    this.save();
+    delete this.hash;
+    return this;
+  } else {
+    this.attempts++;
+    this.save();
+    return false;
+  }
+}
+
+var User = mongoose.model('userModel', userSchema);
+
+User.pre('save', function(next) {
+  this.email = this.email.toLowerCase();
+  var password = this.password || '';
+  delete this.password;
+  if (!this.hash) {
+    // assuming there is no hash yet, set the hash
+    this.hash = bcrypt.hashSync(password, 10);
+  }
+  return next();
+});
+
+
+
 module.exports.gameInstanceModel = mongoose.model('gameInstanceModel', gameInstanceSchema);
-module.exports.userModel = mongoose.model('userModel', userSchema);
+module.exports.userModel = User;
