51ea9fc1dc61b1124f33f6dd7e5e56a09841e22b
Neil Ricci <iccir919@gmail.com>
2017-03-22 14:51:58 -0700

    Players responding component

---
+++ client/src/components/PlayingGameComponents/PlayersResponding.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PlayersResponding = () => (
+  <div id="players-responding">
+    <h3>Please wait...</h3>
+    <h2>Players are responding to prompt</h2>
+  </div>
+)
+
+
+export default PlayersResponding;
