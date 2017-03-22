70716e6655caf555db3c083225ac8d3273813cb1
Scott <smclendening@gmail.com>
2017-03-22 11:24:42 -0700

    Pass down game info from Game component to WaitingRoom component to render number of players and list of players in the Waiting Room

--- client/src/components/WaitingRoom.jsx
+++ client/src/components/WaitingRoom.jsx
@@ -3,8 +3,13 @@ import React from 'react';
 const WaitingRoom = (props) => (
   <div id='waiting-room'>
     <h4>Waiting Room</h4>
+    <h4>Number of Players: {props.numPlayers} / 4</h4>
+    <ol>Players:
+      {props.players.map( (player) => <li>{player}</li>)}
+    </ol>
   </div>
 )
 
-
 export default WaitingRoom;
+
+
