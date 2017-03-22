6fc804fac19ac963a5fa4d2569e8e40149f6647d
Scott <smclendening@gmail.com>
2017-03-22 13:59:41 -0700

    Finish Prompt component, will render given prompt prop which Prompt inherits from PlayingGame component

--- client/src/components/PlayingGame.jsx
+++ client/src/components/PlayingGame.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Prompt from './PlayingGameComponents/Prompt.jsx';
+// this.props.game = game instance object
 
 class PlayingGame extends React.Component{
   constructor(props) {
@@ -7,12 +9,15 @@ class PlayingGame extends React.Component{
   }
 
   render() {
+    var curRound = this.props.game.currentRound;
+    var curPrompt = this.props.game.rounds[curRound].prompt;
+
     return (
       <div id="playing-game">
         <h2>Playing Game</h2>
         <div>
           <h3>Current Judge</h3>
-          <h3>Prompt</h3>
+          <Prompt prompt={curPrompt}/>
           <h3>Timer</h3>
         </div>
         <div>
