20783cfff5896b77a01ae0bbd5ff47bf486ee413
Paul Mills <paulmills@tests-MacBook-Pro-3.local>
2017-03-23 14:51:20 -0700

    (feat) Write AddPlayerToGameInstance function

--- db/db-queries.js
+++ db/db-queries.js
@@ -1,4 +1,4 @@
-var models = require('index.js');
+var models = require('./index.js');
 var games = models.db.collection('gameinstancemodels');
 
 module.exports.retrieveGameInstance = function(gameName) {
@@ -6,4 +6,8 @@ module.exports.retrieveGameInstance = function(gameName) {
   return games.findOne({gameName: gameName});
 };
 
+module.exports.AddPlayerToGameInstance = function(players) {
+
+  return games.update({gameName: gameName}, {players: players});
+};
 
