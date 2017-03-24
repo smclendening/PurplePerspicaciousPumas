82b3d2555077dea21f30ef45adff2a38a08f3cb3
Scott <smclendening@gmail.com>
2017-03-24 10:59:53 -0700

    Add username AJAX call in Lobby to track current user

--- client/src/components/Lobby.jsx
+++ client/src/components/Lobby.jsx
@@ -10,13 +10,16 @@ class Lobby extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      games: null
+      games: null,
+      username: null
     }
     this.getGames = this.getGames.bind(this);
+    this.getUsername = this.getUsername.bind(this);
   }
 
   componentDidMount() {
     this.getGames();
+    this.getUsername();
   }
 
   getGames() {
@@ -36,13 +39,28 @@ class Lobby extends React.Component {
     });
   }
 
+  getUsername() {
+    $.ajax({
+      url: 'http://localhost:3000/username',
+      method: 'GET',
+      headers: {'content-type': 'application/json'},
+      success: (username) => {
+        this.setState({username: username});
+        console.log('got username in Lobby: ', username);
+      },
+      error: (err) => {
+        console.log('error getting username', err);
+      }
+    });
+  }
+
   render() {
     return (
       <div id="lobby">
         <h3>Lobby</h3>
         <h4>Games:</h4>
         {this.state.games && <GameList games={this.state.games} onClick={this.props.route.sendToGame}/>}
-        <CreateGame onClick={this.props.route.sendToGame}/>
+        {this.state.username && <CreateGame onClick={this.props.route.sendToGame}/>}
       </div>
     )
   }
