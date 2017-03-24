ffb38f57b9b36cd838d4650c0603fb5a47df8274
Paul Mills <paulmills@tests-MacBook-Pro-3.local>
2017-03-24 12:22:15 -0700

    (feat) Create score component

---
+++ client/src/components/PlayingGameComponents/Score.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Score extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div id="score">
+      </div>
+    )
+  }
+}
+
+
+
+export default Score;
