986103251893920585d7b00b2c6454e30dd3d4a6
Paul Mills <paulmills@tests-MacBook-Pro-3.local>
2017-03-24 12:42:41 -0700

    (feat) Write out logic to display current scores

--- client/src/components/PlayingGameComponents/Score.jsx
+++ client/src/components/PlayingGameComponents/Score.jsx
@@ -7,8 +7,23 @@ class Score extends React.Component {
   }
 
   render() {
+    let players = this.props.game.players;
+    let rounds = this.props.game.rounds;
+    let scores = {};
+    players.forEach(function(player) {
+      scores[player] = 0;
+    });
+    rounds.forEach(function(round) {
+      if (round.winner.length > 0) {
+        scores[round.winner]++;
+      }
+    });
     return (
       <div id="score">
+      <div><strong>Scores!</strong></div>
+      {Object.keys(scores).map((username) => 
+        <div> {username}: {scores[username]} </div>
+        )}
       </div>
     )
   }
