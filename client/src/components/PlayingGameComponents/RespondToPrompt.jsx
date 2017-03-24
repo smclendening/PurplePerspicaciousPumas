3cbc37a52c5fbadc75c05c2f7c988617ae517a02
Scott <smclendening@gmail.com>
2017-03-24 14:00:22 -0700

    Conditional rendering in submit response component

--- client/src/components/PlayingGameComponents/RespondToPrompt.jsx
+++ client/src/components/PlayingGameComponents/RespondToPrompt.jsx
@@ -4,7 +4,8 @@ class RespondToPrompt extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      response: ''
+      response: '',
+      responded: false
     }
 
     this.handleInputChange = this.handleInputChange.bind(this);
@@ -19,8 +20,16 @@ class RespondToPrompt extends React.Component {
   render() {
     return (
       <div id="choose-username">
-          <b>Player Response: </b><input type="text" value={this.state.value} onChange={this.handleInputChange} />
-          <button onClick={() => this.props.onSubmit(this.state.response)}>Submit</button>
+          {!this.state.responded && <b>Submit Response:</b> && <input type="text" value={this.state.value} onChange={this.handleInputChange} />}
+          {!this.state.responded && <button onClick={() => {
+              console.log('this: ', this);
+              this.setState({
+                responded: true
+              })
+              this.props.handleResponse(this.state.response)
+            }
+          }>Submit</button>}
+          {this.state.responded && <p><b>Your response has been submitted!</b></p>}
       </div>
     )
   }
