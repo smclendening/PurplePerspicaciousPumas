47a44363005d556495e26c5813b7ff3ef04bad81
Paul Mills <paulmills@tests-MacBook-Pro-3.local>
2017-03-23 19:10:24 -0700

    (fix) Update queries to use $set

--- db/db-queries.js
+++ db/db-queries.js
@@ -2,21 +2,20 @@ var models = require('./index.js');
 var games = models.db.collection('gameinstancemodels');
 
 module.exports.retrieveGameInstance = function(gameName) {
-
   return games.findOne({gameName: gameName});
 };
 
 module.exports.addPlayerToGameInstance = function(gameName, players) {
 
-  return games.update({gameName: gameName}, {players: players});
+  return games.update({gameName: gameName}, {$set: {players: players} });
 };
 
 module.exports.setGameInstanceGameStageToPlaying = function(gameName) {
 
-  return games.update({gameName: gameName}, {gameStage: 'playing'});
+  return games.update({gameName: gameName}, { $set: {gameStage: 'playing'} });
 };
 
 module.exports.updateRounds = function(gameName, roundsArray) {
 
-  return games.update({gameName: gameName}, {rounds: roundsArray});
-}
+  return games.update({gameName: gameName}, {  $set: {rounds: roundsArray} });
+};
