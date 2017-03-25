20b0af42c0b034db0a1641729458fff4ec241556
Paul Mills <paulmills@tests-MacBook-Pro-3.local>
2017-03-25 11:44:19 -0700

    (fix) Update indentation in RoundSummary

--- client/src/components/RoundSummary.jsx
+++ client/src/components/RoundSummary.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 const RoundSummary = (props) => (
   <div id="RoundSummary">
   <div>
-  	'Judge: '{props.judge} 'Prompt: ' {props.round.prompt}
+    'Judge: '{props.judge} 'Prompt: ' {props.round.prompt}
   </div>
-  	{props.round.responses.map((response) => (
-  			<div> 
-  				{response[1] === props.round.winner && 'WON: ' }{response[0]} {response[1]}  
-  			</div>
-  		))}
+    {props.round.responses.map((response) => (
+        <div> 
+          {response[1] === props.round.winner && 'WON: ' }{response[0]} {response[1]}  
+        </div>
+      ))}
   </div>
 )
 
