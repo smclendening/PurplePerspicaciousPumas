66ca73fc1e4dd0efa7c00863c293772c9d2c4975
Paul Mills <paulmills@tests-MacBook-Pro-3.local>
2017-03-24 21:30:21 -0700

    (feat) Only show open games in the lobby

--- client/src/components/GameList.jsx
+++ client/src/components/GameList.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import GameListEntry from './GameListEntry.jsx';
 
-const GameList = (props) => (
-  <ul id="GameList">
-    {props.games.map( (game) => <GameListEntry sendToGame={props.sendToGame} name={game.gameName} key={game._id} gameInfo={game} />)}
-  </ul>
-)
+const GameList = (props) => {
+	let openGames = [];
+  props.games.map((game) => {if(game.gameStage === 'waiting'){ openGames.push(game)}});
+	return 	(
+	  <ul id="GameList">
+	    {openGames.map( (game) => <GameListEntry sendToGame={props.sendToGame} name={game.gameName} key={game._id} gameInfo={game} />)}
+	  </ul>
+	)
+}
 
 
 export default GameList;
