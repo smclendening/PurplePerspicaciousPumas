0d2fcdbc3dfacb495f5882f340cb589d5c41b064
Neil Ricci <iccir919@gmail.com>
2017-03-26 13:18:46 -0700

    Implement column spacing, fix unncessary quotation marks

--- client/src/components/RoundSummary.jsx
+++ client/src/components/RoundSummary.jsx
@@ -3,12 +3,12 @@ import { ListGroup, ListGroupItem, PageHeader } from 'react-bootstrap';
 
 const RoundSummary = (props) => (
   <div id="RoundSummary">
-    <h4>'Prompt: ' {props.round.prompt} <small> 'Judge: '{props.judge} </small></h4>
+    <h4>Prompt:  {props.round.prompt} <small> Judge: {props.judge} </small></h4>
     <h5>Responses</h5>
     <ListGroup id="responses-summary">
       {props.round.responses.map((response) => (
         <ListGroupItem id="response-item"> 
-         {response[1] === props.round.winner && 'WON: ' }{response[0]} {response[1]} 
+         {response[1] === props.round.winner && 'WON: '}{response[0]} {response[1]} 
         </ListGroupItem>
       ))}
     </ListGroup>
