8bb1305ed97c7e3ab2d06828fc10fe8e1d137ed5
Neil Ricci <iccir919@gmail.com>
2017-03-25 19:44:53 -0700

    Implement RoundSummary styling

--- client/src/components/RoundSummary.jsx
+++ client/src/components/RoundSummary.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import { ListGroup, ListGroupItem, PageHeader } from 'react-bootstrap';
 
 const RoundSummary = (props) => (
   <div id="RoundSummary">
-  <div>
-    'Judge: '{props.judge} 'Prompt: ' {props.round.prompt}
-  </div>
-    {props.round.responses.map((response) => (
-        <div> 
-          {response[1] === props.round.winner && 'WON: ' }{response[0]} {response[1]}  
-        </div>
+    <h4>'Prompt: ' {props.round.prompt} <small> 'Judge: '{props.judge} </small></h4>
+    <h5>Responses</h5>
+    <ListGroup id="responses-summary">
+      {props.round.responses.map((response) => (
+        <ListGroupItem id="response-item"> 
+         {response[1] === props.round.winner && 'WON: ' }{response[0]} {response[1]} 
+        </ListGroupItem>
       ))}
+    </ListGroup>
   </div>
 )
 
