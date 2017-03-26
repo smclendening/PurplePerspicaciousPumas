8b5c87beb4c8d6c1277f1edb15c74cc8de347e82
Neil Ricci <iccir919@gmail.com>
2017-03-26 13:23:43 -0700

    Delete unnecessary comments and react boootrap elements

--- client/src/components/EndOfGame.jsx
+++ client/src/components/EndOfGame.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Score from './PlayingGameComponents/Score.jsx';
 import RoundSummary from './RoundSummary.jsx';
-import { Col, PageHeader, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Col, PageHeader } from 'react-bootstrap';
 
 const EndOfGame = (props) => {
 	return (
@@ -27,38 +27,3 @@ const EndOfGame = (props) => {
 }
 
 export default EndOfGame;
-
-// <Col id="playing-game">
-//   <PageHeader>{this.props.game.gameName}: <small>Round {this.props.game.currentRound + 1} - Judge: {curJudge}</small></PageHeader>
-//     <Col sm={6} smOffset={3}>
-//       <h4>Scoreboard</h4>
-//       <Score game={this.props.game}/>
-//     </Col>
-//     <Col sm={6} smOffset={3}>
-//       <Prompt prompt={curPrompt}/>
-//     </Col>
-//   <Col sm={6} smOffset={3}>
-//   {stage === 0 && this.state.role === 'judge' && <PlayersResponding />}
-//   {stage === 0 && this.state.role === 'player' && <RespondToPrompt handleResponse={this.props.handleResponse}/>}
-//   {stage === 1 && this.state.role === 'judge' && <ChooseWinner responses={responses} handleJudgeSelection={this.props.handleJudgeSelection}/>}
-//   {stage === 1 && this.state.role === 'player' && <SeeResponses responses={responses}/>}
-//   {stage === 2 && <Winner responses={responses} winner={winner} handleReadyToMoveOn={this.props.handleReadyToMoveOn}/>}
-//   </Col>
-// </Col>
-
-// <div id='EndOfGame'>
-//   <h4>Game Over</h4>
-//   <h4>Final Score</h4>
-//   <Score game={props.game} />
-//   <br />
-//   <RoundSummary round={props.game.rounds[3]} judge={props.game.players[3]}/>
-//   <br />
-//   <RoundSummary round={props.game.rounds[2]} judge={props.game.players[2]}/>
-//   <br />
-//   <RoundSummary round={props.game.rounds[1]} judge={props.game.players[1]}/>
-//   <br />
-//   <RoundSummary round={props.game.rounds[0]} judge={props.game.players[0]}/>
-//   <br />
-//   <button onClick={props.sendToLobby}>Return to Lobby</button>
-// </div>
-
