8799968dd8430bdc9d4c1e9148cb237ac1d0801f
Scott <smclendening@gmail.com>
2017-03-26 13:17:00 -0700

    Fix spacing

--- client/src/components/PlayingGameComponents/Winner.jsx
+++ client/src/components/PlayingGameComponents/Winner.jsx
@@ -5,10 +5,10 @@ const Winner = (props) => (
   <ListGroup id="winner">
     <h4>Results</h4>
   	{props.responses.map((response) => (
-  			<ListGroupItem bsStyle={response[1] === props.winner ? 'success' : 'danger'}> 
-  				<b>{response[1]}:</b> {response[0]} {response[1] === props.winner && <b>(WINNER)</b> } 
-  			</ListGroupItem>
-  		))}
+			<ListGroupItem bsStyle={response[1] === props.winner ? 'success' : 'danger'}> 
+				<b>{response[1]}:</b> {response[0]} {response[1] === props.winner && <b>(WINNER)</b> } 
+			</ListGroupItem>
+		))}
       <br />
     <Button onClick={() => {props.handleReadyToMoveOn()}}>I'm Ready to Move On!</Button>
   </ListGroup>
