ccb4a17c3d484dfef3bcb7801a1f928c7f069aa9
Paul Mills <paulmills@tests-MacBook-Pro-3.local>
2017-03-26 19:40:22 -0700

    (feat) Create PlayerDisconnected component
    Display a message explaining why the user is the lobby

---
+++ client/src/components/PlayerDisconnected.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PlayerDisconnected = () => (
+  <div id="player-disconnected">
+    <h3>Our apologies, a player left your game so it ended early.</h3>
+    <br/>
+  </div>
+)
+
+
+export default PlayerDisconnected;
