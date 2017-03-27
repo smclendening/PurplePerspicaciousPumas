c997f00524a385b8fca1df3b56f95aef090f1e1d
Scott <smclendening@gmail.com>
2017-03-27 11:32:03 -0700

    Remove comments

--- client/src/components/PlayingGameComponents/Winner.jsx
+++ client/src/components/PlayingGameComponents/Winner.jsx
@@ -1,19 +1,6 @@
 import React from 'react';
 import { ListGroup, ListGroupItem, Col, Button } from 'react-bootstrap';
 
-// const Winner = (props) => (
-//   <ListGroup id="winner">
-//     <h4>Results</h4>
-//   	{props.responses.map((response) => (
-// 			<ListGroupItem bsStyle={response[1] === props.winner ? 'success' : 'danger'}> 
-// 				<b>{response[1]}:</b> {response[0]} {response[1] === props.winner && <b>(WINNER)</b> } 
-// 			</ListGroupItem>
-// 		))}
-//       <br />
-//     <Button onClick={() => {props.handleReadyToMoveOn()}}>I'm Ready to Move On!</Button>
-//   </ListGroup>
-// )
-
 class Winner extends React.Component {
   constructor(props) {
     super(props)
