5a16300c5e936f38a75ed4413afbbad55227335c
Scott <smclendening@gmail.com>
2017-03-26 17:50:34 -0700

    Create and implement GameDescription

---
+++ client/src/components/GameDescription.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Col, Panel } from 'react-bootstrap';
+
+const GameDescription = (props) => (
+  <Col id="GameDescription" sm={6} smOffset={3}>
+    <h4>What Is It?</h4>
+    <Panel>
+      <b>Oranges to Oranges</b> is a four-player game where players try to come up 
+      with the funniest responses to thought-provoking questions. Sign up or 
+      log in below!
+    </Panel>
+    <br />
+  </Col>
+)
+
+
+export default GameDescription;
