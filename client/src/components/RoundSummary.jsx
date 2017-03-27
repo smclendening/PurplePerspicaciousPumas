d742c385481128914e18b32445a00fc9914d02f3
Neil Ricci <iccir919@gmail.com>
2017-03-26 19:13:20 -0700

    Rearrange the prompt and judge

--- client/src/components/RoundSummary.jsx
+++ client/src/components/RoundSummary.jsx
@@ -3,8 +3,8 @@ import { ListGroup, ListGroupItem, PageHeader, } from 'react-bootstrap';
 
 const RoundSummary = (props) => (
   <div id="RoundSummary">
-    <h4>Prompt:  {props.round.prompt} <small> Judge: {props.judge} </small></h4>
-    <h5>Responses</h5>
+    <h4> Prompt:  {props.round.prompt}  </h4>
+    <h5> Judge: {props.judge} </h5>
     <ListGroup id="responses-summary">
       {props.round.responses.map((response) => (
         <ListGroupItem bsStyle={response[1] === props.round.winner ? 'success' : 'danger'}> 
